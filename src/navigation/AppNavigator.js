a06e34036462e957f5c7af5d8f3db77139317f1f
Louis Chang <louis.chang.1999@gmail.com>
2021-09-12 17:40:51 +0800

    feat: 0912新增programNameView

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -7,6 +7,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import loginView from "../login/loginView";
 import signUpView from "../login/signUpView";
 import homeView from '../HomeView'
+import programNameView from '../program/ProgramNameView';
 
 const MainStack = createStackNavigator();
 
@@ -20,6 +21,7 @@ const Main = () => {
       <MainStack.Screen name="loginView" component={loginView} />
       <MainStack.Screen name="homeView" component={homeView} />
       <MainStack.Screen name="signUpView" component={signUpView} />
+      <MainStack.Screen name="programNameView" component={programNameView} />
       
 
     </MainStack.Navigator>
