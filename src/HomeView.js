c5cffc5079ace66db5ccf5785819578e4ab13a78
simoncat102 <simoncat102@gmail.com>
2021-09-12 17:37:48 +0800

    0912 首頁行事曆與login調整

--- src/HomeView.js
+++ src/HomeView.js
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 import { Icon } from "react-native-elements";
 import { Button } from "react-native-elements/dist/buttons/Button";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 
 // import { styles } from "../style/loginStyle";
 
@@ -71,13 +72,15 @@ export default function login({ navigation }) {
             style={styles.planImage}
           />
         </View>
+
         {/* 橫線 */}
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <View style={styles.hr} />
         </View>
+        {/* 橫線 */}
 
         {/* 健身規劃 */}
-        <View style={{ flexDirection: "row" , alignSelf: "center",}}>
+        <View style={{ flexDirection: "row", alignSelf: "center" }}>
           <Icon name="calendar" type="font-awesome" color="#2b2b2b" size={20} />
           <Text style={styles.subtitleFont}>{"   "}您個人的健身規劃</Text>
           <TouchableOpacity
@@ -89,10 +92,104 @@ export default function login({ navigation }) {
             <Text style={styles.buttonText}> 新增規劃</Text>
           </TouchableOpacity>
         </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={styles.weekContainer}>
+            <Text style={styles.weekFont}>mon</Text>
+            <Text style={styles.dateFont}>02</Text>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.tranningName}>核心訓練</Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "flex-end" }}>
+            <Text style={styles.tranningTime}>30</Text>
+            <Text style={styles.tranningUnit}>mins</Text>
+          </View>
+
+          <View style={{ flexDirection: "row" }}>
+            <Icon
+              name="check"
+              type="evilicon"
+              color="#2F942F"
+              size={30}
+              marginLeft={40}
+            />
+            <Icon
+              name="sliders"
+              type="font-awesome"
+              color="#2b2b2b"
+              size={20}
+              marginLeft={20}
+            />
+          </View>
+        </View>
         {/* 橫線 */}
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <View style={styles.hr} />
         </View>
+        {/* 橫線 */}
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={styles.weekContainer}>
+            <Text style={styles.weekFont}>tue</Text>
+            <Text style={styles.dateFont}>03</Text>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.restName}>休息日</Text>
+          </View>
+        </View>
+        {/* 橫線 */}
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={styles.hr} />
+        </View>
+        {/* 橫線 */}
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={styles.weekContainer}>
+            <Text style={styles.weekFont}>wed</Text>
+            <Text style={styles.dateFont}>04</Text>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.tranningName}>三頭肌群</Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "flex-end" }}>
+            <Text style={styles.tranningTime}>45</Text>
+            <Text style={styles.tranningUnit}>mins</Text>
+          </View>
+
+          <View style={{ flexDirection: "row" }}>
+            <Icon
+              name="check"
+              type="evilicon"
+              color="#2F942F"
+              size={30}
+              marginLeft={40}
+            />
+            <Icon
+              name="sliders"
+              type="font-awesome"
+              color="#2b2b2b"
+              size={20}
+              marginLeft={20}
+            />
+          </View>
+        </View>
+        {/* 橫線 */}
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={styles.hr} />
+        </View>
+        {/* 橫線 */}
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={styles.weekContainer}>
+            <Text style={styles.weekFont}>thu</Text>
+            <Text style={styles.dateFont}>05</Text>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.restName}>休息日</Text>
+          </View>
+        </View>
+        {/* 橫線 */}
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={styles.hr} />
+        </View>
+        {/* 橫線 */}
       </View>
     </>
   );
@@ -178,5 +275,56 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
     margin: 5,
+  },
+
+  dateFont: {
+    fontFamily: "Verdana",
+    fontSize: 14,
+    color: "#2B2B2B",
+    fontWeight: "bold",
+  },
+  weekFont: {
+    fontFamily: "Verdana",
+    fontSize: 12,
+    color: "#646464",
+    fontWeight: "bold",
+  },
+  weekContainer: {
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  tranningName: {
+    fontFamily: "Verdana",
+    fontSize: 16,
+    color: "#2B2B2B",
+    fontWeight: "bold",
+    marginLeft: 35,
+  },
+  restName: {
+    fontFamily: "Verdana",
+    fontSize: 16,
+    color: "#B9B9B9",
+    fontWeight: "bold",
+    marginLeft: 35,
+  },
+  tranningTime: {
+    fontFamily: "Verdana",
+    fontSize: 16,
+    color: "#2B2B2B",
+    fontWeight: "bold",
+    marginLeft: 25,
+  },
+  tranningUnit: {
+    fontFamily: "Verdana",
+    fontSize: 12,
+    color: "#696969",
+    fontWeight: "bold",
+    marginLeft: 3,
+    alignItems: "flex-end",
+  },
+  checkIcon: {
+    marginLeft: 10,
   },
 });
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -7,6 +7,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import loginView from "../login/loginView";
 import signUpView from "../login/signUpView";
 import homeView from '../HomeView'
+// import AgendaView from '../test/agendaViewTest'
 
 const MainStack = createStackNavigator();
 
@@ -20,7 +21,8 @@ const Main = () => {
       <MainStack.Screen name="loginView" component={loginView} />
       <MainStack.Screen name="homeView" component={homeView} />
       <MainStack.Screen name="signUpView" component={signUpView} />
-      
+      {/* <MainStack.Screen name="AgendaView" component={AgendaView} /> */}
+
 
     </MainStack.Navigator>
   );
